535cdb40f023061ec440b1b651c04a2db61a0b98
ccacca444 <3425761499@qq.com>
2025-08-29 18:41:34 +0800

    更新 aisp.lua

--- aisp.lua
+++ aisp.lua
@@ -226,7 +226,7 @@ local espSettings = {
     BoxThickness = 1,
     HealthBarWidth = 2,  
     HealthBarHeight = 20, 
-    TracerOrigin = "Mouse" 
+    TracerOrigin = "top" 
 }
 
 local function createEsp(player)
